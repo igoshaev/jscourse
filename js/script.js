6a8d99fc1dd17bce24fee87c464ef00a8fedd8ad
igoshaev <nirvanist_2@bk.ru>
2020-12-11 18:23:04 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"Use strict";
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+const isChecked = true,
+      isClosed = false;
+
+console.log(isChecked && isClosed);
