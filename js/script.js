339ce24181ee107fc02097c4cd675514d6293a57
igoshaev <nirvanist_2@bk.ru>
2020-12-11 19:53:51 +0300

    Удалил все лишнее

--- js/script.js
+++ js/script.js
@@ -1,14 +1,2 @@
 "Use strict";
 
-incr++;
-decr--;
-
-console.log(incr);
-console.log(decr);
-
-console.log(5%2);
-
-const isChecked = true,
-      isClosed = false;
-
-console.log(isChecked && isClosed);
