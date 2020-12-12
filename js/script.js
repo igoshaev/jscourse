ac89ae469b6126fd112ff7746de6951f7c0b85fb
igoshaev <nirvanist_2@bk.ru>
2020-12-12 20:44:24 +0300

    Добавлены циклы и условия

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "Use strict";
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +10,34 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?"),
-      b = prompt("На сколько вы оцените его?"),
-      c = prompt("Один из последних просмотренных фильмов?"),
-      d = prompt("На сколько вы оцените его?");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?");
+          b = prompt("На сколько вы оцените его?");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+        alert("Некорректный формат данных");
+    }
+}
 
-console.log(personalMovieDB);
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов")
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель")
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман")
+} else (alert("Ошибка"));
+
+console.log(personalMovieDB)
+
+// const a = prompt("Один из последних просмотренных фильмов?"),
+//       b = prompt("На сколько вы оцените его?"),
+//       c = prompt("Один из последних просмотренных фильмов?"),
+//       d = prompt("На сколько вы оцените его?");
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
